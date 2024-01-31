bb5843c57a0848991f063321d272d5bb60c45138
Aliyan <aliyansiddiqui555@gmail.com>
2024-01-31 10:11:14 +0500

    widow location change

--- my-app/src/Context/SongContext.js
+++ my-app/src/Context/SongContext.js
@@ -11,7 +11,7 @@ const SongContext = createContext();
 
 const SongProvider = ({ children }) => {
   const initialstate = { isError: false, isLoading: false, Songs: [] };
-  const PORT = "http://localhost:5000"
+  const PORT = `${window.location.origin}`
   const [state, dispatch] = useReducer(reducer, initialstate);
   const [playlist, setplaylist] = useState([]);
   const inital = { isLoading: false, RecentlyPlay: [] };
